23c1d0df9895f0aa42b0ad8f74d9152f61260dec
Raul Shahi <rahulfromnepal2013@gmail.com>
2026-06-11 11:18:46 +0200

    added requirements file

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1oNcLnHOrhDUUXprv1YLpXIT6ipE9DqGw
 """
 
-!pip install streamlit
 import streamlit as st
 import numpy as np
 import tensorflow as tf
